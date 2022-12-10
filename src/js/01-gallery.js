e0db8039cccc06f7db93842ab2ff9a5b832d14e5
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-11 01:41:10 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,36 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 
-const galleryBoxrRef = document.querySelector('.gallery');
-const createGalleryMarkup = createCardsImgMarkup(galleryItems);
-galleryBoxrRef.insertAdjacentHTML('afterbegin', createGalleryMarkup);
-galleryBoxrRef.addEventListener("click", onGalleryClick);
+const galleryBoxRef = document.querySelector('.gallery');
+const createGallery = createCardsImg(galleryItems);
+galleryBoxRef.insertAdjacentHTML('afterbegin', createGallery);
+galleryBoxRef.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  const pattern = basicLightbox.create(
-    `<img src="${event.target.dataset.source}"width="1280">`,
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="1280px">`,
     {
-      onShow: (pattern) => {
-        galleryBoxrRef.addEventListener('keydown', onGalleryClickEsc);
+      onShow: (instance) => {
+        galleryBoxRef.addEventListener('keydown', onGalleryClickEsc);
       },
-      onClose: (pattern) => {
-        galleryBoxrRef.removeEventListener("keydown", onGalleryClickEsc);
+      onClose: (instance) => {
+        galleryBoxRef.removeEventListener("keydown", onGalleryClickEsc);
       },
     }
-  );
-    pattern.show();
+  )
+    instance.show();
     
     function onGalleryClickEsc(event) {
     if (event.code === 'Escape') {
-      pattern.close();
+      instance.close();
     }
   }
 }
 
-function createCardsImgMarkup(gallery) {
+function createCardsImg(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`;
