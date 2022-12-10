59053c8a801cb40ce74c4e02f5f8d59fdc338c75
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-10 23:55:33 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryRef = document.createElement('div');
+galleryRef.classList = 'gallery__item';
+
+
+
+function createGallary() {
+const galleryItemRef = document.createElement('a');
+galleryItemRef.classList = 'gallery__link';
+galleryItemRef.setAttribute('href', 'large-image.jpg' );
+galleryItemRef = document.createDocumentFragment();
+    for (let i = 0; i < galleryItems.length; i += 1) {
+        const galleryImgRef = `<img
+        class="gallery__image"
+        src="small-image.jpg"
+        data-source="large-image.jpg"
+        alt="${galleryItems.description}"
+        />`;
+        galleryItemRef.appendChild(galleryImgRef.preview);
+    }
+    galleryRef.appendChild(galleryItemRef);
+};
+
+gallery.insertAdjacentHTML('afterbegin', galleryRef);
+console.log(gallery);
