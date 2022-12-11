31e12ad96447b3acd47518d27a2d4ecd29cf442d
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-11 18:27:15 +0200

    SimpleLightbox instal

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryBoxRef = document.querySelector(".gallery");
+const createGallery = createCardsImg(galleryItems);
+galleryBoxRef.insertAdjacentHTML("afterbegin", createGallery);
+galleryBoxRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="1280px">`,
+    {
+      onShow: (instance) => {
+        galleryBoxRef.addEventListener("keydown", onGalleryClickEsc);
+      },
+      onClose: (instance) => {
+        galleryBoxRef.removeEventListener("keydown", onGalleryClickEsc);
+      },
+    }
+  );
+  instance.show();
+
+  function onGalleryClickEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+function createCardsImg(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`;
+    })
+    .join("");
+}
