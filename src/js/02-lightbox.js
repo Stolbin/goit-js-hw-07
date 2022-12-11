587b36e8d9809d8394eb4c48589cbc7f0da0957a
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-11 19:41:17 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,39 +1,20 @@
 import { galleryItems } from "./gallery-items.js";
 
-const galleryBoxRef = document.querySelector(".gallery");
+const galleryBoxRef = document.querySelector('.gallery');
 const createGallery = createCardsImg(galleryItems);
-galleryBoxRef.insertAdjacentHTML("afterbegin", createGallery);
-galleryBoxRef.addEventListener("click", onGalleryClick);
+galleryBoxRef.insertAdjacentHTML('afterbegin', createGallery);
 
-function onGalleryClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="1280px">`,
-    {
-      onShow: (instance) => {
-        galleryBoxRef.addEventListener("keydown", onGalleryClickEsc);
-      },
-      onClose: (instance) => {
-        galleryBoxRef.removeEventListener("keydown", onGalleryClickEsc);
-      },
-    }
-  );
-  instance.show();
-
-  function onGalleryClickEsc(event) {
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  }
-}
-
-function createCardsImg(gallery) {
-  return gallery
+function createCardsImg(galleryItems) {
+  return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`;
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
     })
     .join("");
 }
+
+const ligthBox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
